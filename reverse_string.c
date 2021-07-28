f91bfa9a486c4066b5868078f4dc8f2edd767aec
kavya554 <72247047+kavya554@users.noreply.github.com>
2021-07-28 11:01:03 +0530

    Add files via upload

---
+++ reverse_string.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void reverse(char *a)
+{
+	if(*a)
+	{
+		reverse(a+1);
+		printf("%c",*a);
+		
+	}
+}
+int main()
+{
+	char a[100];
+	scanf("%[^\n]s",a);
+	reverse(a);
+	return 0;
+	
+}
